41816b23d2482252b8bbe119cebaf0fb4b08c74b
Gajendra K <gajendra.mahadeva@neosoftmail.com>
2022-01-09 18:21:56 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react"
+
+
+export default function Index() {
+
+    const [isVisible, setIsVisible] = useState(false)
+
+  const handleClick = () =>{
+
+
+    console.log('hghg');
+    setIsVisible(!isVisible)
+  }
+
+  return (<div>
+   fgdgfdgf
+    </div>
+
+  )
+}
+
